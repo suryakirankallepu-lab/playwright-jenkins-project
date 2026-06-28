73ec11a3aa9a8898cb3eaa3b7e1fd00a5601e8ab
suryakirankallepu-lab <suryakirankallepu786@gmail.com>
2026-06-28 12:24:34 +0530

    Enabled multi-browser support

--- playwright.config.ts
+++ playwright.config.ts
@@ -19,10 +19,22 @@ export default defineConfig({
     trace: 'retain-on-failure',
   },
 
+  
+
   projects: [
     {
       name: 'chromium',
       use: { ...devices['Desktop Chrome'] },
     },
+    {
+      name: 'firefox',
+      use: { ...devices['Desktop Firefox'] },
+    },
+    {
+      name: 'webkit',
+      use: { ...devices['Desktop Safari'] },
+    },
   ],
+});
+
 });
